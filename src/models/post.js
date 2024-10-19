c9bf3bc71a56860f017b3392934a2fe73fc72d8f
Fabricio Veronez <fabricioveronez@gmail.com>
2024-10-19 15:00:46 -0300

    Adicionando parametrização no banco de dados.

--- src/models/post.js
+++ src/models/post.js
@@ -1,13 +1,27 @@
 const sequelize = require('sequelize');
 
+// Função para converter string em boolean
+const strToBool = (value) => {
+  return value === 'true';
+};
+
 const DB_DATABASE = process.env.DB_DATABASE || "kubedevnews";
 const DB_USERNAME = process.env.DB_USERNAME || "kubedevnews";
 const DB_PASSWORD = process.env.DB_PASSWORD || "Pg#123";
 const DB_HOST = process.env.DB_HOST || "localhost";
+const DB_PORT = parseInt(process.env.DB_PORT, 10) || 5432; 
+const DB_SSL_REQUIRE =  strToBool(process.env.DB_SSL_REQUIRE) || false;
 
 const seque = new sequelize.Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: 'postgres'
+    port: DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: DB_SSL_REQUIRE, 
+        rejectUnauthorized: false 
+      }
+    }
   });
 
 class Post extends sequelize.Model {
