068eed6e2dcc0e394127c24dcc1c9bfd554d525c
Fabricio Veronez <fabricioveronez@gmail.com>
2025-02-08 13:46:09 -0300

    Tratamento novo

--- src/models/post.js
+++ src/models/post.js
@@ -10,19 +10,23 @@ const DB_USERNAME = process.env.DB_USERNAME || "kubedevnews";
 const DB_PASSWORD = process.env.DB_PASSWORD || "Pg#123";
 const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_PORT = parseInt(process.env.DB_PORT, 10) || 5432; 
-const DB_SSL_REQUIRE =  strToBool(process.env.DB_SSL_REQUIRE) || false;
+const DB_SSL_REQUIRE = process.env.DB_SSL_REQUIRE?.toLowerCase() === "true";
 
 const seque = new sequelize.Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: DB_SSL_REQUIRE ? {
-        require: true,
-        rejectUnauthorized: false
-      } : false
+  host: DB_HOST,
+  port: DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: DB_SSL_REQUIRE ? {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  });
+  } : {}
+});
+
+seque.authenticate()
+  .then(() => console.log('✅ Conexão com o banco estabelecida com sucesso!'))
+  .catch(err => console.error('❌ Erro ao conectar ao banco:', err));
 
 class Post extends sequelize.Model {
   
